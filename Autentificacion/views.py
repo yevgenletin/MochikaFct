7dda4fc79ebe8fb3caa1fbcd7696df20be74e1fb
mochika <yevgen.letin@gmail.com>
2022-11-12 22:43:46 +0100

    Rgistro usuario error

--- Autentificacion/views.py
+++ Autentificacion/views.py
@@ -2,17 +2,19 @@ from django.shortcuts import render,redirect
 from django.views.generic import View
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib import messages
 
 
 ##def autentificacion(request):
 ##    return render (request, "Autentificacion/auth.html")
 
+##Renderiza la hoja de registro de usuario.
 class Vregistro(View):
         def get(self, request):
             form = UserCreationForm()
             return render(request, "Autentificacion/auth.html", {"form": form})
             
-
+## Comprobacion del usuario y contraseña.
         def post(self, request):
             form=UserCreationForm(request.POST)
             if form.is_valid():
@@ -20,4 +22,8 @@ class Vregistro(View):
                 login(request, usuario)
                 return redirect("/Home/")
             else:
-                pass
+                for msg in form.error_messages:
+                    print(msg)
+                    messages.error(request, form.error_messages[msg])
+                    return render(request, "Autentificacion/auth.html", {"form": form})
+                
