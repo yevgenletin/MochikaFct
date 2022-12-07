382b35e5eb5843444f3d4a6ba895de607aee50cb
mochika <yevgen.letin@gmail.com>
2022-12-08 00:51:38 +0100

    documentacion 1

--- Favoritos/models.py
+++ Favoritos/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+# Modelo Favoritos
 
 class Favoritos(models.Model):
     id_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
